77bfbd600b8503847c62f4f991a62976fb69cf3e
pierobramanti <piero.bramanti94@gmail.com>
2024-09-05 16:36:18 +0200

    [UPD] created table and cicled hotel's information, ex. done

--- index.php
+++ index.php
@@ -53,13 +53,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Hotel Selector</title>
 </head>
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                
+            <div class="col-12 mt-5">
+            <table class="table border">
+                <thead>
+                    <tr>
+                        <th scope="col">Hotel</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Disponibilità parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro città</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <?php foreach($hotels as $hotel) { ?>
+                            <tr>
+                                <td><?php echo $hotel['name']; ?></td>
+                                <td><?php echo $hotel['description']; ?></td>
+                                <td><?php echo $hotel['parking'] ? 'C\'è Parcheggio' : 'Non c\'è pacheggio'; ?></td>
+                                <td><?php echo $hotel['vote']; ?> <i class="fa-solid fa-star"></i></td>
+                                <td><?php echo $hotel['distance_to_center']; ?> Kilometri</td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+            </table>
             </div>
         </div>
     </div>
